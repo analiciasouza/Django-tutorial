8465fb33d15404723c4ab463871609293ddb902c
analiciasouza <analiciasouza.11@gmail.com>
2025-03-19 11:30:07 -0300

    Atualizando projeto

--- polls/models.py
+++ polls/models.py
@@ -11,7 +11,8 @@ class Question(models.Model):
         return self.question_text
     
     def published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return  now - datetime.timedelta(days=1) <= self.pub_date <= now
     
 
 
@@ -19,6 +20,7 @@ class Choice(models.Model):
       question = models.ForeignKey(Question, on_delete=models.CASCADE)
       choice_text = models.CharField(max_length=255)
       votes = models.IntegerField(default=0)
+
 
       def __str__(self):
           return self.choice_text
--- polls/views.py
+++ polls/views.py
@@ -2,39 +2,62 @@ from django.shortcuts import render, get_object_or_404
 from django.db.models import F
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 from .models import Question, Choice
+from django.utils import timezone
 
 
 # Create your views here.
-def index(request):
-    question_list = Question.objects.all()
-    context = { "question_list" : question_list }
-    return render(request, "polls/index.html" , context)
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "question_list"
     
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question" : question})
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+    
+    def get_questions_no_choice(self):
+        question_choices = Choice.objects.get(question_id=0)
+        return question_choices 
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question" : question})
+
+
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+class ResultsView(generic.DetailView):
+     model = Question
+     template_name = "polls/results.html"
+
+     def get_queryset(self):
+         return Question.objects.filter(pub_date__lte=timezone.now())
+         
+    
 
 def votes(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST["choice"])
-    except (KeyError, Choice.DoesNotExist):
-        context = {
+    context = {
             "question" : question,
             "error_message" : "you didnt select one"
         }
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+        # request.POST permite acessar dados pelo nome chave, ele retornar o ID da escolha selecionada como uma string
+    except (KeyError, Choice.DoesNotExist):
         return render(request,"polls/detail.html", context)
     else:
-        selected_choice.votes = F("votes") + 1
+        selected_choice.votes = F("votes") + 1 # fala para o bando de dados par aumentar o vote em 1
         selected_choice.save()
 
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+        # reverse usa para evitar ter hardcode url na função, pode ser usar a URLconf
 
-
-    
+# sempre retornar um HttpResponseRedirect depois de lidar com dados 
+# em método POST. Isso previne os dados de serem usados duas vezes se 
+# se o usuario clicar em um back button
